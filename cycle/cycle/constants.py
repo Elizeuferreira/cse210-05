af2c4daea8b34dfdc8888d64787cfaf1da188fb1
elizeu <elizeu.aliam@gmail.com>
2022-03-12 18:10:52 -0400

    NewChanged

--- cycle/cycle/constants.py
+++ cycle/cycle/constants.py
@@ -3,13 +3,13 @@ from game.shared.color import Color
 
 COLUMNS = 40
 ROWS = 20
-CELL_SIZE = 15
+CELL_SIZE = 10
 MAX_X = 900
 MAX_Y = 600
-FRAME_RATE = 15
+FRAME_RATE = 10
 FONT_SIZE = 15
-CAPTION = "Snake"
-SNAKE_LENGTH = 8
+CAPTION = "Cycle"
+SNAKE_LENGTH = 1
 WHITE = Color(255, 255, 255)
 RED = Color(255, 0, 0)
 YELLOW = Color(255, 255, 0)
