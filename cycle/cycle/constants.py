1121c9082638f4c9ce6483f7150e3ca1a341157f
elizeu <elizeu.aliam@gmail.com>
2022-03-12 18:19:23 -0400

    add two lenght

--- cycle/cycle/constants.py
+++ cycle/cycle/constants.py
@@ -3,13 +3,13 @@ from game.shared.color import Color
 
 COLUMNS = 40
 ROWS = 20
-CELL_SIZE = 10
+CELL_SIZE = 15
 MAX_X = 900
 MAX_Y = 600
-FRAME_RATE = 10
+FRAME_RATE = 15
 FONT_SIZE = 15
 CAPTION = "Cycle"
-SNAKE_LENGTH = 1
+SNAKE_LENGTH = 2
 WHITE = Color(255, 255, 255)
 RED = Color(255, 0, 0)
 YELLOW = Color(255, 255, 0)
